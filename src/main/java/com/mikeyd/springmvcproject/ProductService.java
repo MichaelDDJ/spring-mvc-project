65f01c974296e4e4cb46eb6b167607696ce81773
Michael Davis <jrmdd1496@gmail.com>
2023-12-21 16:12:10 -0600

    Added search by warranty method.

--- src/main/java/com/mikeyd/springmvcproject/ProductService.java
+++ src/main/java/com/mikeyd/springmvcproject/ProductService.java
@@ -1,7 +1,9 @@
 package com.mikeyd.springmvcproject;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class ProductService {
 
@@ -21,5 +23,33 @@ public class ProductService {
             }
         }
         return null;
+    }
+
+
+    public List<Product> getProductsWithText(String text) {
+
+        List<Product> prods = new ArrayList<>();
+
+        for (Product p : products) {
+            String name = p.getName().toLowerCase();
+            String type = p.getType().toLowerCase();
+            String place = p.getPlace().toLowerCase();
+
+            if (name.contains(text.toLowerCase()) || type.contains(text.toLowerCase()) || place.contains(text.toLowerCase())) {
+                prods.add(p);
+            }
+        }
+        return prods;
+    }
+
+    public List<Product> getProductsInWarranty(){
+        List<Product> prods = new ArrayList<>();
+
+        for (Product p : products) {
+            if(p.getWarranty() >= Year.now().getValue()) {
+                prods.add(p);
+            }
+        }
+        return prods;
     }
 }
--- src/main/java/com/mikeyd/springmvcproject/SpringMvcProjectApplication.java
+++ src/main/java/com/mikeyd/springmvcproject/SpringMvcProjectApplication.java
@@ -3,7 +3,10 @@ package com.mikeyd.springmvcproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.time.Year;
 import java.util.List;
+
 
 @SpringBootApplication
 public class SpringMvcProjectApplication {
@@ -19,7 +22,7 @@ public class SpringMvcProjectApplication {
 		service.addProduct(new Product("Surface", "Desktop", "Living room", 2025));
 		service.addProduct(new Product("Samsung Galaxy", "Phone", "Bedroom", 2024));
 		service.addProduct(new Product("Lenovo", "Monitor", "Office", 2026));
-		service.addProduct(new Product("Acer", "Monitor", "Brown Table", 2026));
+		service.addProduct(new Product("Asus", "Monitor", "Brown Table", 2026));
 
 		List<Product> products = service.getAllProducts();
 		for(Product product : products) {
@@ -28,7 +31,26 @@ public class SpringMvcProjectApplication {
 
 		//line to divide results
 		System.out.println("==================================================================");
+		System.out.println("Search by name");
 
 		System.out.println(service.getProductByName("MacBook"));
+
+		System.out.println("==================================================================");
+		System.out.println("Search by any text");
+
+		List<Product> prods = service.getProductsWithText("Monitor");
+		for(Product product : prods) {
+			System.out.println(product);
+		}
+
+		System.out.println("==================================================================");
+		System.out.println("Search by warranty");
+
+		List<Product> warrantyProducts = service.getProductsInWarranty();
+        for (Product product : warrantyProducts) {
+            System.out.println(product);
+        }
+
+
 	}
 }
